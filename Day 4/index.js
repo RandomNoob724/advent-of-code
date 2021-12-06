32a65abef56e578a4abf9c35b7625c75bc71b231
Emil Persson <emilper1999@gmail.com>
2021-12-06 20:11:27 +0100

    Solved first part of day 4

--- Day 4/index.js
+++ Day 4/index.js
@@ -1,28 +1,99 @@
-const fs = require('fs');
-
+const fs = require("fs");
 
 // 1. Read the file input.txt and print its content to the console.
 // 2. Create a function that takes an array of numbers and returns a bingo card.
-fs.readFile('input.txt', 'utf8', (err, data) => {
-    data = data.split('\n');
-    // Extract the drawn numbers from the input file
-    let drawnNumbers = data.shift().split(',');
+fs.readFile("input.txt", "utf8", (err, data) => {
+  data = data.split("\n");
+  // Extract the drawn numbers from the input file
+  let drawnNumbers = data.shift().split(",");
 
-    let cards = createBingoCards(data);
+  let cards = createBingoCards(data);
+  let [winningCard, lastDrawnNumber] = findWinningBoard(cards, drawnNumbers);
+  console.log(cards.findIndex((item) => item === winningCard));
+
+  let sum = findSumOfCard(winningCard);
+    console.log(lastDrawnNumber);
+  console.log(sum * lastDrawnNumber);
 });
 
 const createBingoCards = (lines) => {
-    let bingoCards = [];
-    let card = [];
-    lines.forEach((line) => {
-        line = line.split(' ');
-        line = line.filter(item => item !== '');
-        if(line.length === 0){
-            bingoCards.push(card);
-            card = [];
-        } else {
-            card.push(line);
+  let bingoCards = [];
+  let card = [];
+  lines.forEach((line) => {
+    line = line.split(" ");
+    line = line.filter((item) => item !== "");
+    if (line.length === 0) {
+      bingoCards.push(card);
+      card = [];
+    } else {
+      card.push(line);
+    }
+  });
+  return bingoCards;
+};
+
+const findWinningBoard = (cards, drawnNumbers) => {
+  let winningCard = null;
+  for (let drawnNumber of drawnNumbers) {
+    for (let card of cards) {
+      for (let line of card) {
+        if (line.includes(drawnNumber)) {
+          const numberIndex = line.findIndex((item) => item == drawnNumber);
+          line[numberIndex] = "X";
+        }
+      }
+      if (card.length > 0) {
+        winningCard = checkVertically(card);
+        winningCard = checkHorizontally(card);
+        if (winningCard) {
+          return [winningCard, drawnNumber];
+        }
+      }
+    }
+  }
+};
+
+const checkVertically = (card) => {
+  for (let i = 0; i < 5; i++) {
+    let count = 0;
+    for (let j = 0; j < 5; j++) {
+      if (card[j][i] == "X") {
+        count++;
+      } else {
+        count = 0;
+      }
+      if (count == 5) {
+        return card;
+      }
+    }
+  }
+};
+
+const checkHorizontally = (card) => {
+  for (let i = 0; i < 5; i++) {
+    let count = 0;
+    for (let j = 0; j < 5; j++) {
+      if (card[i][j] == "X") {
+        count++;
+      } else {
+        count = 0;
+      }
+      if (count == 5) {
+        console.log(card);
+        return card;
+      }
+    }
+  }
+};
+
+const findSumOfCard = (card) => {
+  let sum = 0;
+  card.forEach((line) => {
+    line.forEach((item) => {
+        if (item != "X") {
+            sum += parseInt(item);
         }
     });
-    return bingoCards;
+  });
+  return sum;
 };
